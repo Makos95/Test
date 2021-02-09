7ae56d91dcce9342c10d99b0127d65ffa557f849
Harris Dailianis <harris@gloot.com>
2021-02-09 19:04:26 +0100

    Fixed styling for the navbar, removed unessesery styling, mobile fix

--- src/pages/startpage/index.jsx
+++ src/pages/startpage/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Navbar from '../../components/navbar.js';
+import React from "react";
+import Navbar from "../../components/navbar.js";
 export default function StartPage() {
-    return (
-        <>
-            <div> <h1>  </h1> <Navbar /> </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
 }
-
