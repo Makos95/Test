1333c716f5d2b8e03a6212cd7a226cb4487ab215
Rana Alchachino <ranaalchachino@Ranas-MacBook-Air.local>
2021-02-09 18:11:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StartPage from './pages/startpage';
+import Produkter from './pages/produkter';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path='/' component={StartPage} />
+      <Route path='/produkter' component={Produkter} />
+    </Router>
   );
 }
 
